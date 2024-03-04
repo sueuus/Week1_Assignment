b284463b96318243ddce05fb7f563f312c5f7f8a
sueuus <162073188+sueuus@users.noreply.github.com>
2024-03-04 15:54:08 -0500

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,14 +1,27 @@
+#Question 1
 def count_vowels(word):
-    vowels = 'aeiouAEIOU'
-    return sum(1 for letter in word if letter in vowels)
+    vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    count = 0
+    for i in word:
+         if i in vowel:
+              count += 1
+    return count
 
+print(count_vowels("2024NYUDSBootcamp"))
+
+#Question 2
 animals = ['tiger', 'elephant', 'monkey', 'zebra', 'panther']
 for animal in animals:
     print(animal.upper())
 
+#Question 3
 for number in range(1, 21):
     print(f"{number} is {'even' if number % 2 == 0 else 'odd'}")
 
+#Question 4:
 def sum_of_int(a,b):
-    return a+b
+    return a + b
+ #I know there is no need to print it out... I just want to check if I am correct :)
+print(sum_of_int(1,7)) 
+
 
